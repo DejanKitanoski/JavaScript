41b43eb63f320184b69fed44bb16b6de1901ed5c
DejanKitanoski <117692954+DejanKitanoski@users.noreply.github.com>
2024-11-19 17:40:25 +0100

    Add files via upload

---
+++ Js-1/script.js
@@ -0,0 +1,12 @@
+console.log("Делење на 3 и 5");
+var broj = 345;
+
+if (broj % 3 == 0 && broj % 5 == 0) {
+  console.log("fizzbuzz")
+}
+ else if (broj % 3 == 0) {
+  console.log("fizz");
+} 
+else if (broj % 5 == 0) {
+  console.log("buzz");
+}
